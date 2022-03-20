a28fe0a5c4f2da72011d5d1746e1e9d8d03c2df5
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-03-20 00:10:53 -0300

    import dotenv

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-require("dotenv").config();
+import dotenv from 'dotenv';
 import express from "express";
 import path from "path";
+
+dotenv.config();
 
 const app = express();
 app.set('view engine', 'ejs');
